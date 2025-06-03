3ccacb9e671f87361434b590de1a956608ee7f31
JonatanPalavecino <jnpr1992@hotmail.com>
2025-06-02 21:28:34 -0300

    Se arregla problema con variable de entorno

--- src/services/sendEmail.services.ts
+++ src/services/sendEmail.services.ts
@@ -2,14 +2,20 @@ import { render } from "@react-email/render";
 import { Resend } from "resend";
 import React from "react";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export const sendEmail = async <T extends Record<string, any>>(
     email: Array<string>,
     subject: string,
     Template: React.FC<T>,
     props: T
 ) => {
+
+    const apiKey = process.env.RESEND_API_KEY
+
+    if (!apiKey) {
+        throw Error("Falta la variable de entorno de RESEND")
+    }
+
+    const resend = new Resend(apiKey);
 
     try {
         
